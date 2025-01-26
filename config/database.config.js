826f6ecd505721affcffdf0baeccb1fde1252a1c
Bekbolat <forestnatplay@yandex.ru>
2025-01-26 22:41:15 +0500

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb+srv://zbekxzz:Zbekxzz3@utopianunits.itnvrbl.mongodb.net/utopianUnits'
+    url: 'mongodb+srv://zbekxzz:Zbekxzz3@cluster0.xqfhxdo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 }
